e1fc93971460124d83f33a106a4179f1ad456d24
DHPARK <pdh4182@gmail.com>
2022-10-23 09:42:01 +0900

    setting.py

--- portfolioPage/router/url.js
+++ portfolioPage/router/url.js
@@ -2,4 +2,8 @@ module.exports = (router) => {
   router.route("/").get((req, res) => {
     res.render("index.ejs");
   });
+
+  router.route("/handsomeHack").get((req, res) => {
+    res.render("handsomeHack.ejs");
+  });
 };

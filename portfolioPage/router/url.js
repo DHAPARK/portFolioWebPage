4e69806b2fc1778ca42151b4dd9d1ce127e2fff0
DHPARK <pdh4182@gmail.com>
2022-10-23 14:00:04 +0900

    일단일단락?

--- portfolioPage/router/url.js
+++ portfolioPage/router/url.js
@@ -6,4 +6,15 @@ module.exports = (router) => {
   router.route("/handsomeHack").get((req, res) => {
     res.render("handsomeHack.ejs");
   });
+
+  router.route("/eveStation").get((req, res) => {
+    res.render("eveStation.ejs");
+  });
+
+  router.route("/kbsc").get((req, res) => {
+    res.render("kbsc");
+  });
+  router.route("/capstone").get((req, res) => {
+    res.render("capstone");
+  });
 };
